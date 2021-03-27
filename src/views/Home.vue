d73269d02edbc6e028d73486df4cb93167d60ed4
xxxx <squall0609.kanon@gmail.com>
2021-03-27 10:32:18 +0900

    vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,113 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        
-      </v-col>
+  <v-card
+    max-width="400"
+    class="mx-auto"
+  >
+    <v-system-bar
+      color="pink darken-2"
+      dark
+    >
+      <v-spacer></v-spacer>
 
-      <v-col class="mb-4">
+      <v-icon>mdi-window-minimize</v-icon>
 
-      </v-col>
+      <v-icon>mdi-window-maximize</v-icon>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
+      <v-icon>mdi-close</v-icon>
+    </v-system-bar>
 
+    <v-app-bar
+      dark
+      color="pink"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-        <v-row justify="center">
+      <v-toolbar-title>アカウント一覧</v-toolbar-title>
 
-        </v-row>
-      </v-col>
+      <v-spacer></v-spacer>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
 
-        <v-row justify="center">
+    <v-container>
+      <v-row dense>
+        <v-col cols="12">
+          <v-card
+            color="#385F73"
+            dark
+          >
+            <v-card-title class="headline">
+              アカウント名
+            </v-card-title>
 
-        </v-row>
-      </v-col>
+            <v-card-subtitle>Bio</v-card-subtitle>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
+            <v-card-actions>
+              <v-btn text>
+                コメント
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
 
+        <v-col
+          v-for="(item, i) in items"
+          :key="i"
+          cols="12"
+        >
+          <v-card
+            :color="item.color"
+            dark
+          >
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title
+                  class="headline"
+                  v-text="item.title"
+                ></v-card-title>
 
-        <v-row justify="center">
+                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
 
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+                <v-card-actions>
+                  <v-btn
+                    v-if="item.artist === 'Ellie Goulding'"
+                    class="ml-2 mt-3"
+                    fab
+                    icon
+                    height="40px"
+                    right
+                    width="40px"
+                  >
+                    <v-icon>mdi-play</v-icon>
+                  </v-btn>
+
+                  <v-btn
+                    v-else
+                    class="ml-2 mt-5"
+                    outlined
+                    rounded
+                    small
+                  >
+                    START RADIO
+                  </v-btn>
+                </v-card-actions>
+              </div>
+
+              <v-avatar
+                class="ma-3"
+                size="125"
+                tile
+              >
+                <v-img :src="icon_src"></v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
@@ -52,6 +118,23 @@ export default {
   name: 'Home',
   components: {
     // HelloWorld
+  },
+  data: () => ({
+    items: [
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People',
+      },
+      {
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Halcyon Days',
+        artist: 'Ellie Goulding',
+      },
+    ],
+  icon_src: require("../assets/img/84540173.png"),
+    }),
   }
-}
 </script>
