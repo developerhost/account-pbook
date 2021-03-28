ac0a415b6bc98a089775dd862a3476c9b3a0977c
xxxx <squall0609.kanon@gmail.com>
2021-03-28 11:17:36 +0900

    first commit

---
+++ node.js
@@ -0,0 +1,16 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: 'FahZjs4X8G6qEAfyHXH59sEW1',
+  cunsumer_secret: 'q5tXdwWHXwenpB2aPZm4DRlpacJlFZmLl2jV7adntZcvVUESnu',
+  access_token_key: '1056207757315993600-kUC9UlrRPUxyQcRzGbSOl3oiI4FXfw',
+  access_token_secret: 'yaVQONBHtTUdQh9cK89dEeN8gNifQc538UW97cOKWtA8V'
+});
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+    
+  }
+});
