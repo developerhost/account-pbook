8d2f41347e667c24b1ad07d62d1642b5747f0ae9
xxxx <squall0609.kanon@gmail.com>
2021-03-29 16:42:49 +0900

    request

--- functions/index.js
+++ functions/index.js
@@ -8,19 +8,41 @@ const functions = require("firebase-functions");
 //   response.send("テスト!");
 // });
 
-exports.helloOnCall = functions.https.onCall((data, context) => {
+exports.helloOnCall = functions.https.onRequest((req, res) => {
   const request = require("request");
 
-  const url = "https://api.twitter.com/1.1/users/show.json" + data;
+  const URL = "https://api.twitter.com/1.1/users/show.json";
 
-  const res = request("GET", url);
-  const resJson = res.getBody("utf-8");
+  request.get({
+    uri: URL,
+    headers: {
+      "Content-type": "application/json",
+      // eslint-disable-next-line quote-props
+      Authorization:
+        // eslint-disable-next-line max-len
+        "Bearer AAAAAAAAAAAAAAAAAAAAAOAtOAEAAAAASgAspot9JypeUtd53mMZOO4%2F7KY%3D4tL8qdCCdO4bJIUOCNz1nICYbigdAV9rGdDFAZjpCrCiMsdUEF",
+    },
+    qs: {
+      screen_name: "sikaotokoawo",
+    },
+    json: true,
+  },
+  function(error, req, data) {
+    if (!error) {
+      console.log(data.name);
+    } else {
+      console.log(error);
+    }
+  }
+  );
 
-  const obj = JSON.parse(resJson); // オブジェクトをJSONとして文字列として出力
-  // parse=JSON形式の文字列を文字列として置き換える
+  // const resJson = res.getBody("utf-8");
 
-  const returnRes = {
-    name: obj.data.name,
-  };
-  return returnRes;
+  // const obj = JSON.parse(resJson); // オブジェクトをJSONとして文字列として出力
+  // // parse=JSON形式の文字列を文字列として置き換える
+
+  // const returnRes = {
+  //   name: obj.data.name,
+  // };
+  // return returnRes;
 });
